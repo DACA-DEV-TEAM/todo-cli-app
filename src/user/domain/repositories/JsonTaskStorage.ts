d114900041409a892a4207243238bf1a57e17c1e
Carlos <metawiser@gmail.com>
2023-04-19 12:50:47 +0200

    TA-5 configuring files structure

---
+++ src/user/domain/repositories/JsonTaskStorage.ts
@@ -0,0 +1,51 @@
+/* eslint-disable unused-imports/no-unused-vars */
+/* eslint-disable @typescript-eslint/require-await */
+import TaskStorage from "../../infrastructure/TaskStorage";
+import { readJsonFile, writeJsonFile } from "../../util/JsonFileUtil";
+import { Task } from "../entities/Task";
+
+class JsonTaskStorage implements TaskStorage {
+	constructor(public path: string) {}
+
+	async createTask(task: Task): Promise<Task> {
+		const data = await readJsonFile(this.path);
+		data.push(task);
+		await writeJsonFile(this.path, data);
+
+		return task;
+	}
+
+	async updateTask(id: string, updatedTaskData: Partial<Task>): Promise<boolean> {
+		const data = await readJsonFile(this.path);
+		const taskToUpdate = data.findIndex((task) => task.id === id);
+		if (taskToUpdate !== -1) {
+			data[taskToUpdate] = { ...data[taskToUpdate], ...updatedTaskData };
+
+			return true;
+		}
+		throw new Error("Not found");
+	}
+
+	async deleteTask(id: string): Promise<boolean> {
+		throw new Error("Method not implemented.");
+	}
+
+	async listTasks(): Promise<Task[]> {
+		const data = await readJsonFile(this.path);
+
+		return data;
+	}
+
+	async get(id: string): Promise<Task> {
+		const data = await readJsonFile(this.path);
+		const task = data.find((task) => task.id === id);
+
+		if (task === undefined) {
+			throw new Error("Task Not Found");
+		}
+
+		return task;
+	}
+}
+
+export default JsonTaskStorage;
---
+++ src/user/infrastructure/TaskStorage.ts
@@ -0,0 +1,11 @@
+import { Task } from "../domain/entities/Task";
+
+interface TaskStorage {
+	createTask(task: Task): Promise<Task>;
+	updateTask(id: string, updatedTaskData: Partial<Task>): Promise<boolean>;
+	deleteTask(id: string): Promise<boolean>;
+	listTasks(): Promise<Task[]>;
+	get(id: string): Promise<Task>;
+}
+
+export default TaskStorage;
