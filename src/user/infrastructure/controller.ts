77756baf74c09b82cd5bbee30e74f08cd3fd596e
Danny Mejia <dannym.mejia@gmail.com>
2023-04-20 16:30:45 +0200

    TA-9 implemented createTask, listTasks

--- src/user/infrastructure/controller.ts
+++ src/user/infrastructure/controller.ts
@@ -1,11 +1,31 @@
 import JsonTaskStorage from "../domain/repositories/JsonTaskStorage";
+import { readImput } from "./inquirer/inquirerMenu";
 import Service from "./service";
-
-const path = "";
+//TODO meter path en una variable de entorno
+const path = "./src/jsonDb/db.json";
 const jsonTaskStorage = new JsonTaskStorage(path);
 const service = new Service(jsonTaskStorage);
 
-const createTask = async () => {
-	await service.createTask(" ");
+const createTask = async (): Promise<void> => {
+	const desc = await readImput("description");
+	await service.createTask(desc);
+};
+//TODO si se agrega colors, poner a varios colores para los estados
+const listTasks = async (): Promise<void> => {
+	(await service.listTasks()).forEach((task, i) => {
+		const idx = `${i + 1}`;
+		const { description, status } = task;
+
+		console.log(`${idx} ${description} :: ${status}`);
+	});
+};
+const updateTask = async (): Promise<void> => {
 	throw new Error("");
 };
+const searchTask = async (): Promise<void> => {
+	throw new Error("");
+};
+const deleteTask = async (): Promise<void> => {
+	throw new Error("");
+};
+export { createTask, deleteTask, listTasks, searchTask, updateTask };
