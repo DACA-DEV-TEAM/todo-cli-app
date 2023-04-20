197814a65c43e3a964c553e6766f98dd5c3da422
Carlos <metawiser@gmail.com>
2023-04-20 13:20:07 +0200

    TA-10 Class Service created

---
+++ src/user/infrastructure/service.ts
@@ -0,0 +1,33 @@
+import TaskStorage from "../../user/domain/interface/TaskStorage";
+import { Task } from "../domain/entities/Task";
+
+class service {
+	constructor(private _db: TaskStorage) {}
+
+	public set db(db: TaskStorage) {
+		this._db = db;
+	}
+
+	async createTask(desc: string): Promise<void> {
+		const task = new Task(desc);
+		await this._db.createTask(task);
+	}
+
+	async listTasks(): Promise<Task[]> {
+		return await this._db.listTasks();
+	}
+
+	async updateTask(id: string, partial: object): Promise<boolean> {
+		const partialTask: Partial<Task> = partial;
+
+		return await this._db.updateTask(id, partialTask);
+	}
+
+	async searchTask(id: string): Promise<Task> {
+		return await this._db.get(id);
+	}
+
+	async deleteTask(id: string): Promise<boolean> {
+		return await this._db.deleteTask(id);
+	}
+}
