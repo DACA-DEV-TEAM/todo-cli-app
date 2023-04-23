2aa58a4df3d792aa84cf952104d79ea5685d7f15
Danny <dannym.mejia@gmail.com>
2023-04-23 10:15:53 +0200

    TA-9 deleteTask

--- src/user/infrastructure/controller.ts
+++ src/user/infrastructure/controller.ts
@@ -25,29 +25,44 @@ interface Partial {
 	status?: string;
 }
 const updateTask = async (): Promise<void> => {
-	// Creo el objeto vacio
+	const taskList = await service.listTasks();
+	if (taskList.length === 0) {
+		throw new Error("There is not tasks");
+	}
 	const partial: Partial = {};
-	// Muestro la lista para obtener el id
-	const idx = await showTasks(await service.listTasks());
-	// le agrego la key id
+
+	const idx = await showTasks(taskList);
 	partial.id = idx;
 	const confirmDesc = await confirmOperation("Do you want to update description?");
 	if (confirmDesc) {
 		partial.description = await readImput("Add a new description:");
-	} /*
-	const confirmStatus = await confirmOperation("Do you want to update description?");
+	}
+	const confirmStatus = await confirmOperation("Do you want to update status?");
 
-	const status = await readImput("status");
-	*/
-	console.log(partial);
-	await service.updateTask(idx, partial);
+	//TODO Falta implementar la selección del status en el inquire
+	if (confirmStatus) {
+		partial.status = await readImput("Add new status");
+	}
 
-	//throw new Error("");
+	const updateTask = await service.updateTask(idx, partial);
+	if (updateTask) {
+		console.log("Task updated succesfully");
+	}
 };
 const searchTask = async (): Promise<void> => {
 	throw new Error("");
 };
 const deleteTask = async (): Promise<void> => {
-	throw new Error("");
+	const taskList = await service.listTasks();
+	if (taskList.length === 0) {
+		throw new Error("There is not tasks");
+	}
+	const idx = await showTasks(taskList);
+	const task = await service.searchTask(idx);
+	const confirmDelete = await confirmOperation(`Do you want to delete? -- ${task.description}`);
+	if (confirmDelete) {
+		await service.deleteTask(idx);
+		console.log("\n Task deleted successfully");
+	}
 };
 export { createTask, deleteTask, listTasks, searchTask, updateTask };
