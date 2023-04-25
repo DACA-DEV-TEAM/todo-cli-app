ad97ac50cb9ac23a5311dca2ccb1295d57e86a19
Danny Mejia <dannym.mejia@gmail.com>
2023-04-25 12:08:36 +0200

    develop merge update with TA-30

--- src/user/infrastructure/service.ts
+++ src/user/infrastructure/service.ts
@@ -1,9 +1,12 @@
 import bcrypt from "bcrypt";
+import * as dotenv from "dotenv";
 
 import User from "../../user/domain/entities/User";
 import TaskStorage from "../../user/domain/interface/TaskStorage";
 import UserStorage from "../../user/domain/interface/UserStorage";
 import { Task } from "../domain/entities/Task";
+
+dotenv.config();
 
 class service {
 	private _userID = "";
@@ -25,19 +28,35 @@ class service {
 	}
 
 	async authenticate(username: string, password: string): Promise<boolean> {
-		const user = await this._userDb.getUserByUsername(username);
-		const isValidPassword = await bcrypt.compare(password, user.password);
+		try {
+			const user = await this._userDb.getUserByUsername(username);
+			const isValidPassword = await bcrypt.compare(password, user.password);
 
-		if (isValidPassword) {
-			this._userID = user._id;
+			if (isValidPassword) {
+				this._userID = user._id;
+			}
+
+			return isValidPassword;
+		} catch (error: unknown) {
+			if (process.env.NODE_ENV === "dev") {
+				if (error instanceof Error) {
+					console.error(error.message);
+				} else {
+					console.error("An error occurred");
+				}
+			}
+
+			return false;
 		}
-
-		return isValidPassword;
 	}
 
 	async createTask(desc: string): Promise<void> {
 		const task = new Task(desc, this._userID);
 		await this._db.createTask(task);
+	}
+
+	async userTasks(): Promise<Task[]> {
+		return await this._db.getUserTasks(this._userID);
 	}
 
 	async listTasks(): Promise<Task[]> {
@@ -50,8 +69,18 @@ class service {
 		return await this._db.updateTask(id, partialTask);
 	}
 
-	async searchTask(id: string): Promise<Task> {
-		return await this._db.get(id);
+	async searchTask(
+		id: string
+	): Promise<{ description: string; status: string; startTime: string; endTime: string | null }> {
+		const task = await this._db.get(id);
+		const parsedObj = {
+			description: task.description,
+			status: task.status,
+			startTime: task.startTime.toLocaleString(),
+			endTime: task.endTime ? task.endTime.toLocaleString() : null,
+		};
+
+		return parsedObj;
 	}
 
 	async deleteTask(id: string): Promise<boolean> {
